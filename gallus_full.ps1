44987f05e88807000947bde589829c886faa0516
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-03-26 11:23:29 +0100

    Ajout de la récupération des parties de Gallus

--- gallus_full.ps1
+++ gallus_full.ps1
@@ -1,4 +1,43 @@
-# Download tools
+# List of Gallus parts
+$baseURL="https://"
+$parts=@(
+"conf/Bootstrap.ini",
+"conf/CustomSettings.ini",
+"conf/Gallus_ts.xml",
+"scripts/AuditingScript.ps1",
+"scripts/CopyAuditingFiles.ps1",
+"scripts/CopyGallusFiles.wsf",
+"scripts/Eternilab.png",
+"scripts/GHI.csv",
+"scripts/GenerateGHITable.ps1",
+"scripts/HardeningScript.ps1",
+"scripts/ReportScript.js",
+"scripts/ReportStyle.css",
+"scripts/Variables.ps1",
+"scripts/tsorter.min.js",
+"1-gallus_download_tools.ps1",
+"2-gallus_setup_tools.ps1",
+"3-gallus_download_windows_image.ps1",
+"4-gallus_extract_windows_image.ps1",
+"5-gallus_download_HardeningKitty.ps1",
+"6-gallus_cleanup_MDT.ps1",
+"7-gallus_run_MDT.ps1",
+"8-gallus_build_USB_media.ps1",
+"README.md"
+)
+
+# Cleanup potential old files
+Remove-Item -Verbose -Recurse -Force -Path $PWD\conf
+Remove-Item -Verbose -Recurse -Force -Path $PWD\scripts
+# Create directories
+New-Item -Verbose -ItemType Directory -Path $PWD\conf
+New-Item -Verbose -ItemType Directory -Path $PWD\scripts
+# Download parts
+foreach ($part in $parts) {
+  Invoke-WebRequest -Verbose -Uri $baseURL$part -OutFile $PWD\$part"
+}
+
+# 1
 $PWD\1-gallus_download_tools.ps1
 # 2
 $PWD\2-gallus_setup_tools.ps1
@@ -8,9 +47,10 @@ $PWD\3-gallus_download_windows_image.ps1
 $PWD\4-gallus_extract_windows_image.ps1
 # 5
 $PWD\5-gallus_download_HardeningKitty.ps1
-# Clean-up previous build files
+# 6
 $PWD\6-gallus_cleanup_MDT.ps1
 # 7
 $PWD\7-gallus_run_MDT.ps1
 # 8
-$PWD\8-gallus_build_USB_media.ps1
+$cleUSB = Read-Host -Prompt 'Veuillez saisir le périphérique USB où déployer l''installateur (ex: "F:")'
+$PWD\8-gallus_build_USB_media.ps1 $cleUSB
