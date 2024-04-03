8e13fb1ea8ca660dd7fb628ec579a3ca702b0357
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-03 10:42:52 +0200

    Correction du nom du binaire adkpe

--- 2-gallus_setup_tools.ps1
+++ 2-gallus_setup_tools.ps1
@@ -1,7 +1,7 @@
 # Install ADK | need to verify when finished
 $PWD\adk.exe /features OptionId.DeploymentTools OptionId.ICDConfigurationDesigner /quiet /ceip off
 # Install ADK PE | need to verify when finished
-$PWD\adk.exe /features OptionId.WindowsPreinstallationEnvironment /quiet /ceip off
+$PWD\adkpe.exe /features OptionId.WindowsPreinstallationEnvironment /quiet /ceip off
 # Install MDT
 $PWD\mdt.msi /passive
 # Patch MDT, x86 ...
