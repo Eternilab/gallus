9abc5ddc624f78a5db63da43fb5e2f563d667041
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-05 16:50:31 +0200

    Màj chemin toolsdl

--- 1-gallus_download_tools.ps1
+++ 1-gallus_download_tools.ps1
@@ -1,14 +1,14 @@
 # Cleanup potential old files
-Remove-Item -Verbose -Recurse -Force -Path $PWD\tmpdl
+Remove-Item -Verbose -Recurse -Force -Path $PWD\toolsdl
 # Create directory
-New-Item -Verbose -ItemType Directory -Path $PWD\tmpdl
+New-Item -Verbose -ItemType Directory -Path $PWD\toolsdl
 # Disable progress bar to speedup download
 $ProgressPreference = 'SilentlyContinue'
 # Download ADK
-Invoke-WebRequest -Verbose -Uri https://go.microsoft.com/fwlink/?linkid=2196127 -OutFile $PWD\tmpdl\adk.exe
+Invoke-WebRequest -Verbose -Uri https://go.microsoft.com/fwlink/?linkid=2196127 -OutFile $PWD\toolsdl\adk.exe
 # Download ADK PE
-Invoke-WebRequest -Verbose -Uri https://go.microsoft.com/fwlink/?linkid=2196224 -OutFile $PWD\tmpdl\adkpe.exe
+Invoke-WebRequest -Verbose -Uri https://go.microsoft.com/fwlink/?linkid=2196224 -OutFile $PWD\toolsdl\adkpe.exe
 # Download MDT
-Invoke-WebRequest -Verbose -Uri https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x64.msi -OutFile $PWD\tmpdl\mdt.msi
+Invoke-WebRequest -Verbose -Uri https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x64.msi -OutFile $PWD\toolsdl\mdt.msi
 # Re-enable progress bar
 $ProgressPreference = 'Continue'
--- 2-gallus_setup_tools.ps1
+++ 2-gallus_setup_tools.ps1
@@ -1,8 +1,8 @@
 # Install ADK | TODO need to verify when finished
-$PWD\adk.exe /features OptionId.DeploymentTools OptionId.ICDConfigurationDesigner /quiet /ceip off
+$PWD\toolsdl\adk.exe /features OptionId.DeploymentTools OptionId.ICDConfigurationDesigner /quiet /ceip off
 # Install ADK PE | TODO need to verify when finished
-$PWD\adkpe.exe /features OptionId.WindowsPreinstallationEnvironment /quiet /ceip off
+$PWD\toolsdl\adkpe.exe /features OptionId.WindowsPreinstallationEnvironment /quiet /ceip off
 # Install MDT
-$PWD\mdt.msi /passive
+$PWD\toolsdl\mdt.msi /passive
 # Patch MDT, x86 ... Maybe ?
 #TODO MDT needs patch ? Test in a new machine to find out
