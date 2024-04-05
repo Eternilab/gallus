68c1cbe183682fdb41cfbfc331d03ebf2f1d42bd
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-05 17:01:15 +0200

    Finalisation de l'extraction de l'ESD
    1) Finalisation de la phase 4 pour extraire de l'ESD récupéré chez
    Microsoft l'ensemble de tous les éléments nécessaires :
    - Fichiers d'installation
    - sources/boot.wim (WinPE/Environnement d'installation)
    - sources/install.wim (Image windows)
    
    2) Homogénisation de la syntaxe d'appel à DISM

--- 9-gallus_build_USB_media.ps1
+++ 9-gallus_build_USB_media.ps1
@@ -3,6 +3,6 @@ param(
      [string]$DestDrive
  )
 robocopy "GMedia\Content" "$DestDrive" /s /max:3800000000
-Dism /Split-Image /ImageFile:"GMedia\Content\Deploy\Operating Systems\Windows11x64_Pro_en-US_22H2\sources\install.wim" /SWMFile:"$DestDrive\Deploy\Operating Systems\Windows11x64_Pro_en-US_22H2\sources\install.swm" /FileSize:3800
+DISM /Split-Image /ImageFile:"GMedia\Content\Deploy\Operating Systems\Windows11x64_Pro_en-US_22H2\sources\install.wim" /SWMFile:"$DestDrive\Deploy\Operating Systems\Windows11x64_Pro_en-US_22H2\sources\install.swm" /FileSize:3800
 ((Get-Content -Path "$DestDrive\Deploy\Control\OperatingSystems.xml") -replace 'install.wim','install.swm') | Set-Content -Path "$DestDrive\Deploy\Control\OperatingSystems.xml" -Verbose
 ((Get-Content -Path "$DestDrive\Deploy\Control\GALLUS\Unattend.xml") -replace 'install.wim','install.swm') | Set-Content -Path "$DestDrive\Deploy\Control\GALLUS\Unattend.xml" -Verbose
