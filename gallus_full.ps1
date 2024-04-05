5164a6c8429f18ea0031fffdedc0f46ae0d3e341
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-05 18:23:52 +0200

    Màj avec URL GitHub

--- gallus_full.ps1
+++ gallus_full.ps1
@@ -1,5 +1,5 @@
 # List of Gallus parts
-$baseURL="https://gitlab.unmondelibre.fr/gallus/core/-/raw/main/"
+$baseURL="https://raw.githubusercontent.com/Eternilab/gallus/main/"
 $parts=@(
 "conf/Bootstrap.ini",
 "conf/CustomSettings.ini",
