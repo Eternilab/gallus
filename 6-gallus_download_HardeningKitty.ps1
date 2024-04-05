d91814bff1236a955395797265be0c2e20901253
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-05 18:05:55 +0200

    Revert "Màj HK, dl sans spécifier de nom de fichier"
    This reverts commit cbcd3e4c76bf9c282aa62405b928e81a3e090536.

--- 6-gallus_download_HardeningKitty.ps1
+++ 6-gallus_download_HardeningKitty.ps1
@@ -3,6 +3,6 @@ Remove-Item -Verbose -Recurse -Force -Path $PWD\hkdl
 # Create directory
 New-Item -Verbose -ItemType Directory -Path $PWD\hkdl
 #Download HardeningKitty psm and hardening list
-Invoke-WebRequest -Verbose -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/HardeningKitty.psm1 -OutFile $PWD\hkdl\
-Invoke-WebRequest -Verbose -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/HardeningKitty.psd1 -OutFile $PWD\hkdl\
-Invoke-WebRequest -Verbose -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/lists/finding_list_cis_microsoft_windows_11_enterprise_22h2_machine.csv -OutFile $PWD\hkdl\
+Invoke-WebRequest -Verbose -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/HardeningKitty.psm1 -OutFile $PWD\hkdl\HardeningKitty.psm1
+Invoke-WebRequest -Verbose -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/HardeningKitty.psd1 -OutFile $PWD\hkdl\HardeningKitty.psd1
+Invoke-WebRequest -Verbose -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/lists/finding_list_cis_microsoft_windows_11_enterprise_22h2_machine.csv -OutFile $PWD\hkdl\finding_list_cis_microsoft_windows_11_enterprise_22h2_machine.csv
