4c70478a49cb64b8d32dce1cfe6d510b7727b53e
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-09 17:32:27 +0200

    Correction chemin ADKs

--- 2-gallus_setup_tools.ps1
+++ 2-gallus_setup_tools.ps1
@@ -1,8 +1,8 @@
 # Install ADK
-Start-Process -PassThru -Wait -FilePath $PWD\adksetup.exe -ArgumentList "/features OptionId.DeploymentTools OptionId.ICDConfigurationDesigner /quiet /ceip off"
+Start-Process -PassThru -Wait -FilePath $PWD\toolsdl\adksetup.exe -ArgumentList "/features OptionId.DeploymentTools OptionId.ICDConfigurationDesigner /quiet /ceip off"
 Write-Host "Windows ADK à été installé"
 # Install ADK WinPE
-Start-Process -PassThru -Wait -FilePath $PWD\adkwinpesetup.exe -ArgumentList "/features OptionId.WindowsPreinstallationEnvironment /quiet /ceip off"
+Start-Process -PassThru -Wait -FilePath $PWD\toolsdl\adkwinpesetup.exe -ArgumentList "/features OptionId.WindowsPreinstallationEnvironment /quiet /ceip off"
 Write-Host "Windows ADP WinPE à été installé"
 # Install MDT
 Start-Process -PassThru -Wait -FilePath $PWD\toolsdl\mdt.msi -ArgumentList "/passive"
