000174808599c25029e474324fc5f44258baf8a3
Dionys Colson <dionys.colson@eternilab.com>
2024-04-15 17:28:16 +0200

    feat(scripts/ReportScript.js): Remanié le code Javascript pour qu'il trie par défaut selon la sévérité
    il trie par défaut selon la sévérité à l'ouverture du document, et j'ai donné un ordre de tri particulier pour la sévérité, mettant une plus haute sévérité plus haut dans le tableau.

--- scripts/ReportScript.js
+++ scripts/ReportScript.js
@@ -15,47 +15,72 @@ window.onload = () => {
     // l'index de la colonne et le sens de tri.
     var comparateur = function(columnIndex, estAscendant) {
 
-        // a et b ne sont pas passés explicitement, ils sont égaux aux cases N et N+1 de
-	// la colonne que l'on trie. Sort() les passera implicitement lorsqu'il sera appelé.
+        // Si c'est la 4ème colonne du tableau (la colonne Sévérité) :
+        if (columnIndex == 3) {
+            // on crée un dictionnaire ordonné qui fait correspondre un ordre aux différentes Sévérités.
+            // les lignes dont la sévérité est "High" seront affichées tout en haut, suivies des lignes "Medium", etc.
+            customSortOrder = {'Passed':3, 'Low':2, 'Medium':1, 'High':0};
+
+            // a et b ne sont pas passés explicitement, ils sont égaux aux cases N et N+1 de
+            // la colonne que l'on trie. Sort() les passera implicitement lorsqu'il sera appelé.
+            return function(a, b) {
+
+                // Cette fonction est appelée directement (quelques lignes plus bas qu'ici !).
+                // En fonction du sens de tri, v1 et v2 seront égaux à a et b, interchangeablement.
+                // On passe à v1 et v2 les valeurs des cellules a puis b respectivement si c'est ascendant, ou b puis a si c'est descendant
+                return function(v1, v2) {
+                    // La ligne Sévérité est particulière car on connaît chacune de ses valeurs possibles. On les passe donc comme clé à customSortOrder qui nous répond avec la valeur associée à cette clé.
+                    return (customSortOrder[v1] - customSortOrder[v2]);
+                // On passe à v1 et v2 les valeurs des cellules a puis b respectivement si c'est ascendant, ou b puis a si c'est descendant
+                }(getValeurCellule(estAscendant ? a : b, columnIndex), getValeurCellule(estAscendant ? b : a, columnIndex));
+            };
+        };
+
+        // Si ce n'est pas la colonne Sévérité :
         return function(a, b) {
 
-            // Cette fonction est appelée directement (quelques lignes plus bas qu'ici !).
-	    // En fonction du sens de tri, v1 et v2 seront égaux à a et b, interchangeablement.
             return function(v1, v2) {
-
                 // Si v1 et v2 sont numériques : on fait simplement v1 - v2, et ça renvoie un
-		// nombre négatif (v1 vient avant v2), 0 (c'est égal) ou nombre positif (v2 vient après v1).
-		// Si v1 et/ou v2 est une / sont des string(s), on utilise localeCompare() pour
-		// renvoyer -1 (v1 vient avant v2), 0 (c'est égal) ou 1 (v2 vient après v1).
+                // nombre négatif (v1 vient avant v2), 0 (c'est égal) ou nombre positif (v2 vient après v1).
+                // Si v1 et/ou v2 est une / sont des string(s), on utilise localeCompare() pour
+                // renvoyer -1 (v1 vient avant v2), 0 (c'est égal) ou 1 (v2 vient après v1).
                 return (v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2))
                     ? v1 - v2
                     : v1.toString().localeCompare(v2, undefined, {numeric: true});
-	    // On passe à v1 et v2 les valeurs des cellules a puis b respectivement si c'est ascendant, ou b puis a si c'est descendant
+            // On passe à v1 et v2 les valeurs des cellules a puis b respectivement si c'est ascendant, ou b puis a si c'est descendant
             }(getValeurCellule(estAscendant ? a : b, columnIndex), getValeurCellule(estAscendant ? b : a, columnIndex));
-        }
+        };
     };
 
-    // on ajoute un évènement click au titre de chaque colonne
-    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-	// pour chaque ligne à partir de la seconde
+    // fonction qui, pour une colonne donnée, appelle le comparateur pour chaque ligne avec sa suivante.
+    function sortColumn(th){
+        // Pour chaque ligne de cette colonne à partir de la deuxième :
         Array.from(dataTable.querySelectorAll('tr:nth-child(n+2)'))
-	    // On la compare avec la suivante en l'envoyant dans comparateur.
-            // Pour ce faire, on passe à comparateur l'index de la colonne (en prenant l'index de th dans
-	    // la liste des enfants de sa ligne parente), ainsi que la propriété ascendante / descendante
-	    // du tri, qu'on inverse en même temps !
-	    // Comparateur, avec ces paramètres, crée et renvoie la fonction (a,b) que sort() utilise.
+        // On la compare avec la suivante en l'envoyant dans comparateur.
+        // Pour ce faire, on passe à comparateur l'index de la colonne (en prenant l'index de th dans
+        // la liste des enfants de sa ligne parente), ainsi que la propriété ascendante / descendante
+        // du tri, qu'on inverse en même temps !
+        // Comparateur, avec ces paramètres, crée et renvoie la fonction (a,b) que sort() utilise.
             .sort(comparateur(Array.from(th.parentNode.children).indexOf(th), this.estAscendant = !this.estAscendant))
             .forEach(tr => dataTable.appendChild(tr));
+    }
+
+    // on ajoute un évènement click au titre de chaque colonne qui appelle la fonction sortColumn
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+        sortColumn(th);
     })));
 
+    // On trie par défaut la table avec la 4ème colonne, la Sévérité.
+    sortColumn(document.querySelector('th:nth-of-type(4)'))
+
     // Enfin, on crée une liste des lignes de la table...
-    var rows = dataTable.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+    var rows = dataTable.querySelectorAll('tr:nth-child(n+2)');//.getElementsByTagName("tr");
 
     // Et on itère à travers chacune de ces lignes pour changer sa couleur en fonction de son statut :
     // Réussi ou Échoué (Gravité basse - moyenne - haute).
     Array.from(rows).forEach((row) => {
         if (getValeurCellule(row, 6) == 'Failed'){
-	    switch (getValeurCellule(row, 7)) {
+            switch (getValeurCellule(row, 7)) {
                 case 'High':
                     row.className = "red";
                     break;
