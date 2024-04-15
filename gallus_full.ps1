02d929638c66f8c3d0c85fa501584b6c7d30c0d7
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-15 11:49:20 +0200

    Remplacement ù par u

--- gallus_full.ps1
+++ gallus_full.ps1
@@ -63,5 +63,5 @@ Write-Host ""
 Write-Host -ForegroundColor Green "Creation du media d'installation sur support de stockage amovible"
 Write-Host -ForegroundColor Green "!!! Attention les fichiers presents sur le support vont etre supprimes !!!"
 Write-Host -ForegroundColor Green "Si vous voulez interrompre le processus utilisez le raccourcis clavier Ctrl + C"
-$cleUSB = Read-Host -Prompt 'Veuillez saisir le nom du support de stockage amovible où deployer l''installateur (ex: "F:")'
+$cleUSB = Read-Host -Prompt 'Veuillez saisir le nom du support de stockage amovible ou deployer l''installateur (ex: "F:")'
 &$PWD\9-gallus_build_USB_media.ps1 $cleUSB
