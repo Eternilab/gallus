f3ee2ac0e02f951d30f878c48939be5062647b8d
Jérôme Maurin <jerome.maurin@eternilab.com>
2024-04-16 10:58:58 +0200

    Màj messages d'information dans phases 9

--- 9-gallus_build_USB_media.ps1
+++ 9-gallus_build_USB_media.ps1
@@ -17,10 +17,10 @@ $null = New-Partition -DiskPath $disk -UseMaximumSize -DriveLetter "$DestDrive"
 $null = Format-Volume -DriveLetter $DestDrive -FileSystem FAT32
 Write-Host -ForegroundColor Green "9.2 - Generation du media amovible d'installation sur ${DestDrive}:"
 ROBOCOPY "GMedia\Content" "${DestDrive}:" /nfl /ndl /njh /njs /nc /ns /np /s /max:3800000000
+# ROBOCOPY ajoute un saut de ligne à la sortie standard
 DISM /Quiet /Split-Image /ImageFile:"GMedia\Content\Deploy\Operating Systems\Win11x64_EntN_en-US_22H2\sources\install.wim" /SWMFile:"${DestDrive}:\Deploy\Operating Systems\Win11x64_EntN_en-US_22H2\sources\install.swm" /FileSize:3800
 ((Get-Content -Path "${DestDrive}:\Deploy\Control\OperatingSystems.xml") -replace 'install.wim','install.swm') | Set-Content -Path "${DestDrive}:\Deploy\Control\OperatingSystems.xml"
 ((Get-Content -Path "${DestDrive}:\Deploy\Control\GALLUS\Unattend.xml") -replace 'install.wim','install.swm') | Set-Content -Path "${DestDrive}:\Deploy\Control\GALLUS\Unattend.xml"
-Write-Host ""
 Write-Host -ForegroundColor Green "Le media d'installation ${DestDrive}: est pret."
 write-host -foregroundcolor green "Il peut etre utilise pour installer Windows 11 Enterprise N 22h2 sur un machine x64 uefi sans besoin de connexion internet"
 write-host -foregroundcolor green "Le systeme d'exploitation sera durcis (securise) automatiquement au premier demarrage"
