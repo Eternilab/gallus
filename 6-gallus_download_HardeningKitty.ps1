227df18eb544aa062535062a625b34baaea7d46d
Jérôme Maurin <jerome.maurin@eternilab.com>
2025-02-28 16:25:53 +0100

    Removed old scripts and set bootstrap to tag v0.3

--- 6-gallus_download_HardeningKitty.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host -ForegroundColor Green "6 - Telechargement de l'outil HardeningKitty et de la liste de durcissement CIS Windows 11 Enterprise 22H2"
-# Cleanup potential old files
-Remove-Item -Recurse -Force -Path $PWD\hkdl -ErrorAction SilentlyContinue
-# Create directory
-$null = New-Item -ItemType Directory -Path $PWD\hkdl
-#Download HardeningKitty psm and hardening list
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/HardeningKitty.psm1 -OutFile $PWD\hkdl\HardeningKitty.psm1
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/HardeningKitty.psd1 -OutFile $PWD\hkdl\HardeningKitty.psd1
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/scipag/HardeningKitty/master/lists/finding_list_cis_microsoft_windows_11_enterprise_22h2_machine.csv -OutFile $PWD\hkdl\finding_list_cis_microsoft_windows_11_enterprise_22h2_machine.csv
--- 7-gallus_cleanup_MDT.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host -ForegroundColor Green "7 - Nettoyage d'eventuels anciens projets MDT"
-# Remove PSDrives
-Remove-PSDrive -Name "GALLUSMEDIA" -ErrorAction SilentlyContinue
-Remove-PSDrive -Name "DS001" -ErrorAction SilentlyContinue
-# Delete build directories DSGallus and GMedia
-Remove-Item -Recurse -Force -Path "$PWD/GMedia" -ErrorAction SilentlyContinue
-Remove-Item -Recurse -Force -Path "$PWD/DSGallus" -ErrorAction SilentlyContinue
