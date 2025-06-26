9a9be01047e34ee77ce8cef71bfcc13864b552f8
Jérôme Maurin <jerome.maurin@eternilab.com>
2025-06-26 16:23:53 +0200

    Màj ADK et ADK WinPE : Test de la version 24h2

--- gallus.ps1
+++ gallus.ps1
@@ -59,9 +59,9 @@ function download_tools{
   # Disable progress bar to speedup download
   $ProgressPreference = 'SilentlyContinue'
   # Download ADK
-  Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=2196127 -OutFile $PWD\toolsdl\adksetup.exe
+  Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=2289980 -OutFile $PWD\toolsdl\adksetup.exe
   # Download ADK PE
-  Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=2196224 -OutFile $PWD\toolsdl\adkwinpesetup.exe
+  Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=2289981 -OutFile $PWD\toolsdl\adkwinpesetup.exe
   # Download MDT
   Invoke-WebRequest -Uri https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x64.msi -OutFile $PWD\toolsdl\mdt.msi
   # Re-enable progress bar
