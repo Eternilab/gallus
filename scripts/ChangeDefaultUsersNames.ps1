1838d3b13621f5f9b53583f57d12aed36b13a769
Jérôme Maurin <jerome.maurin@eternilab.com>
2025-06-26 14:43:52 +0200

    ChangeDefaultUsersNames.ps1 : usage méthode robuste pour toutes les langues système
    On utilise les SID plutot que les noms d'utilisateurs, car ces derniers peuvent changer d'une langue à l'autre.

--- scripts/ChangeDefaultUsersNames.ps1
+++ scripts/ChangeDefaultUsersNames.ps1
@@ -1,3 +1,4 @@
-(Get-WmiObject Win32_UserAccount -Filter "name='Administrator'").Rename("$TSEnv:AdminName")
-(Get-WmiObject Win32_UserAccount -Filter "name='Guest'").Rename("$TSEnv:GuestName")
+$MachineSID=(Get-LocalUser | Select-Object -First 1).SID.Value -replace '-\d+$',''
+(Get-WmiObject Win32_UserAccount -Filter ("SID='" + $MachineSID + "-500'")).Rename("$TSEnv:AdminName")
+(Get-WmiObject Win32_UserAccount -Filter ("SID='" + $MachineSID + "-501'")).Rename("$TSEnv:GuestName")
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name 'DefaultUserName' -Value "$TSEnv:AdminName"
